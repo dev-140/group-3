957ced04b0023e57fb243d7e48bb11ce259242da
rox <roxmarzan14@gmail.com>
2022-03-26 02:12:37 +0800

    new section added

--- js/app.js
+++ js/app.js
@@ -9,13 +9,23 @@ $(document).ready(function(){
         }
     });
 
-    $(window).scroll(function() {
-        var height = $(window).scrollTop();
+    // $(window).scroll(function() {
+    //     var height = $(window).scrollTop();
+    //     var aboutLinkTop = $('.aboutLink').offset().top;
     
-        if(height >= 1054) {
-            $('.aboutLink').addClass('active');
-        }else{
-            $('.aboutLink').removeClass('active');
+    //     if(height >= 1186) {
+    //         $('.aboutLink').addClass('active');
+    //     }else{
+    //         $('.aboutLink').removeClass('active');
+    //     }
+        
+    //     console.log(aboutLinkTop);
+       
+    // });
+
+    $(document).on('scroll', function() {
+        if ($(this).scrollTop() >= $('#theTarget').position().top) {
+          console.log('I have been reached');
         }
-    });
+    })
 });
