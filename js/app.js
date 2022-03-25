43fe54fdb542b704f22024af3269a003654035dd
rox <roxmarzan14@gmail.com>
2022-03-26 01:14:08 +0800

    new updates on navbar

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
+    $(window).scroll(function() {
+        var height = $(window).scrollTop();
+    
+        if(height === 0){
+            $('.nav-main-container').removeClass('active');
+        }else{
+            $('.nav-main-container').addClass('active');
+        }
+    });
 
+    $(window).scroll(function() {
+        var height = $(window).scrollTop();
+    
+        if(height === 1054) {
+            $('.aboutLink').addClass('active');
+        }else{
+            $('.aboutLink').removeClass('active');
+        }
+    });
 });
