b3f24f86ffea7fcffda29ccee5ce13c1a926290a
rox <roxmarzan14@gmail.com>
2022-04-30 14:08:12 +0800

    updates

--- js/app.js
+++ js/app.js
@@ -9,29 +9,15 @@ $(document).ready(function(){
         }
     });
 
-    // $(window).scroll(function() {
-    //     var height = $(window).scrollTop();
-    //     var aboutLinkTop = $('.aboutLink').offset().top;
+    // $.fn.isInViewport = function() {
+    //     var elementTop = $(this).offset().top;
+    //     var elementBottom = elementTop + $(this).outerHeight();
     
-    //     if(height >= 1186) {
-    //         $('.aboutLink').addClass('active');
-    //     }else{
-    //         $('.aboutLink').removeClass('active');
-    //     }
-        
-    //     console.log(aboutLinkTop);
-       
-    // });
-
-    $.fn.isInViewport = function() {
-        var elementTop = $(this).offset().top;
-        var elementBottom = elementTop + $(this).outerHeight();
+    //     var viewportTop = $(window).scrollTop();
+    //     var viewportBottom = viewportTop + $(window).height();
     
-        var viewportTop = $(window).scrollTop();
-        var viewportBottom = viewportTop + $(window).height();
-    
-        return elementBottom > viewportTop && elementTop < viewportBottom;
-    };
+    //     return elementBottom > viewportTop && elementTop < viewportBottom;
+    // };
 
     $(window).on('resize scroll', function() {
         if ($('.mission-main-container').isInViewport()) {
@@ -50,31 +36,7 @@ $(document).ready(function(){
             $('.nav-container ul li a').removeClass('active');
         }
     });  
-    setInterval(function () {
-        var beepOne = $('#beep')[0];
-        $('.play-bg-music').on('click' , (e)=> {
-            beepOne.play(); 
-            beepOne.volume = 0.5;
-            $('.play-bg-music').addClass('mute');
-            $('.mute').removeClass('play-bg-music');
-        });
-
-        $('.mute').on('click' , (e)=> {
-            beepOne.volume = 0.5;
-            beepOne.pause(); // Stop playing
-            beepOne.currentTime = 0; // Reset time
-            $('.mute').addClass('play-bg-music');
-            $('.play-bg-music').removeClass('mute');
-        });
     
-    },);
-    
-    $(document).on('scroll', function() {
-        if ($(this).scrollTop() >= $('#theTarget').position().top) {
-          console.log('I have been reached');
-        }
-    })
-
     $('.contact-btn').on('click', (e)=> {
         $('.contact-pop-up-modal-main-container').addClass('active');
         $('.contact-pop-up-modal-main-container').removeClass('oppacity');
